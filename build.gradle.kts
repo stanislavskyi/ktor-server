e8cbe98f3bc30e06f3fbfadcb95743c48a384bcd
stanislavskyi <amadeusmozart1a@gmail.com>
2024-10-08 01:35:25 +0300

    hardcode logback

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 
 val kotlin_version: String by project
-val logback_version: String by project
+
 
 plugins {
     kotlin("jvm") version "2.0.20"
@@ -24,7 +24,7 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("ch.qos.logback:logback-classic:1.5.8")
     implementation("io.ktor:ktor-server-config-yaml")
     testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
