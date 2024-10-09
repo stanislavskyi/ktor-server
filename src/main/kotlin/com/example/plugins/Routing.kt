386ff2a69fce5b829b5e203ff2be2c9643f6c682
stanislavskyi <amadeusmozart1a@gmail.com>
2024-10-09 18:04:45 +0300

    Added post "/save-token"

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,6 +1,8 @@
 package com.example.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -8,6 +10,19 @@ fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        post("/save-token") {
+            val request = call.receive<Map<String, String>>()
+            val token = request["token"]
+            val userId = request["userId"]
+
+            if (token != null && userId != null) {
+                //saveTokenToDatabase(userId, token)
+                call.respond(HttpStatusCode.OK, "Token saved successfully")
+            } else {
+                call.respond(HttpStatusCode.BadRequest, "Missing token or userId")
+            }
         }
     }
 }
