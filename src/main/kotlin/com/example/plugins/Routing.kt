937bbdbebc91e24f1ced9524bf87c89d11e88acf
stanislavskyi <amadeusmozart1a@gmail.com>
2024-10-14 22:47:57 +0300

    add FCM

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -5,9 +5,7 @@ import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import io.ktor.serialization.gson.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -18,10 +16,6 @@ import org.slf4j.LoggerFactory
 private val log: Logger = LoggerFactory.getLogger("MyPlugin")
 
 fun Application.configureRouting() {
-    install(ContentNegotiation) {
-        gson()
-    }
-
     routing {
         get("/") {
             call.respondText("Hello World!")
