5f3469d00b4316514d79b8c809811b407ea502d8
stanislavskyi <amadeusmozart1a@gmail.com>
2024-10-14 21:59:11 +0300

    add FCM

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,13 +1,7 @@
 package com.example.plugins
 
 import com.google.firebase.cloud.FirestoreClient
-import io.ktor.client.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.serialization.gson.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -42,34 +36,34 @@ fun Application.configureRouting() {
 
                 //saveTokenToDatabase(request.userId, request.token)
 
-                val client = HttpClient(CIO) {
-                    install(ContentNegotiation) {
-                        gson()
-                    }
-                }
-
-                val response: HttpResponse = client.post("https://fcm.googleapis.com/v1/projects/musicplayerapplication-be7c8/messages:send") {
-                    headers {
-                        append("Authorization", "Bearer ya29.a0AcM612xukjw9lJwVYG3AadUW-LzxOkyCjf_nte4ZMJrUT5Q6i1LJOTC9CUXX62vnCSSOaC5Ef23ryj80vD4v3yuXFgPSeVjg5zs1jipGHT07Qf9y9cazSRbbXNlxG1QtAiQKSbbHt0j6jRBsiZiEmdTqG23MM2m3gUdiFbo0aCgYKAQsSARMSFQHGX2MidvhmMejlauPJMa4v5cNITg0175")
-                        append("Content-Type", "application/json")
-                    }
-                    setBody(
-                        """
-            {
-              "message": {
-                "token": "${request.token}",
-                "notification": {
-                  "title": "title",
-                  "body": "body"
-                }
-              }
-            }
-            """.trimIndent()
-                    )
-                }
-
-                log.info("RESPONSE FCM: ${response.bodyAsText()}")
-                client.close()
+//                val client = HttpClient(CIO) {
+//                    install(ContentNegotiation) {
+//                        gson()
+//                    }
+//                }
+//
+//                val response: HttpResponse = client.post("https://fcm.googleapis.com/v1/projects/musicplayerapplication-be7c8/messages:send") {
+//                    headers {
+//                        append("Authorization", "Bearer ya29.a0AcM612xukjw9lJwVYG3AadUW-LzxOkyCjf_nte4ZMJrUT5Q6i1LJOTC9CUXX62vnCSSOaC5Ef23ryj80vD4v3yuXFgPSeVjg5zs1jipGHT07Qf9y9cazSRbbXNlxG1QtAiQKSbbHt0j6jRBsiZiEmdTqG23MM2m3gUdiFbo0aCgYKAQsSARMSFQHGX2MidvhmMejlauPJMa4v5cNITg0175")
+//                        append("Content-Type", "application/json")
+//                    }
+//                    setBody(
+//                        """
+//            {
+//              "message": {
+//                "token": "${request.token}",
+//                "notification": {
+//                  "title": "title",
+//                  "body": "body"
+//                }
+//              }
+//            }
+//            """.trimIndent()
+//                    )
+//                }
+//
+//                log.info("RESPONSE FCM: ${response.bodyAsText()}")
+//                client.close()
 
 
             } catch (e: Exception) {
